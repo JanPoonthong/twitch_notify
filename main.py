29b83e03aa73f101a8ea031a55f2504730cfab1e
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-06 10:07:10 +0700

    Except connection error

--- main.py
+++ main.py
@@ -11,30 +11,33 @@ class Notifly:
         self.secret = ""
 
     def check(self):
-        auth_params = {'client_id': self.client_id,
-                       'client_secret': self.secret,
-                       'grant_type': 'client_credentials'
-                       }
-        aut_call = requests.post(url=self.auth, params=auth_params)
-        token = aut_call.json()['access_token']
-        head = {'Client-ID': self.client_id,
-                'Authorization':  "Bearer " + token
-                }
-        content_json = requests.get(self.url, headers=head).json()["data"]
+        try:
+            auth_params = {'client_id': self.client_id,
+                           'client_secret': self.secret,
+                           'grant_type': 'client_credentials'
+                           }
+            aut_call = requests.post(url=self.auth, params=auth_params)
+            token = aut_call.json()['access_token']
+            head = {'Client-ID': self.client_id,
+                    'Authorization':  "Bearer " + token
+                    }
+            content_json = requests.get(self.url, headers=head).json()["data"]
 
-        if content_json:
-            content_json = content_json[0]
-            if content_json['type'] == 'live':
-                os.system("mpg123 " + "sound.mp3")
-                print("https://www.twitch.tv/strager")
-                return True
+            if content_json:
+                content_json = content_json[0]
+                if content_json['type'] == 'live':
+                    os.system("mpg123 " + "sound.mp3")
+                    print("https://www.twitch.tv/strager")
+                    return True
+                else:
+                    return False
             else:
                 return False
-        else:
-            return False
+        except ConnectionError:
+            self.check()
 
 
 if __name__ == "__main__":
     while True:
         print(Notifly().check())
-        time.sleep(3)
+        time.sleep(1)
