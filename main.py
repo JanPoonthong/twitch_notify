623a482f13bf10292874098839a39406f7828e8e
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-15 07:16:33 +0700

    Request token only once

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
+import argparse
 import os
 import sys
-import argparse
+
+import requests
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -11,7 +12,7 @@ SECRET = os.environ['SECRET']
 CLIENT_ID = os.environ['CLIENT_ID']
 
 
-class Notifly:
+class Notify:
     def __init__(self, username):
         self.username = username
         self.url = f"https://api.twitch.tv/helix/streams?user_login={self.username}"
@@ -19,30 +20,28 @@ class Notifly:
         self.client_id = CLIENT_ID
         self.secret = SECRET
 
-    def check(self):
+    def get_token(self):
+        auth_params = {'client_id': self.client_id,
+                       'client_secret': self.secret,
+                       'grant_type': 'client_credentials'
+                       }
+        aut_call = requests.post(url=self.auth, params=auth_params)
+        token = aut_call.json()['access_token']
+        head = {'Client-ID': self.client_id,
+                'Authorization': "Bearer " + token
+                }
+        return head
+
+    def check(self, head):
         try:
-            auth_params = {'client_id': self.client_id,
-                           'client_secret': self.secret,
-                           'grant_type': 'client_credentials'
-                           }
-            aut_call = requests.post(url=self.auth, params=auth_params)
-            token = aut_call.json()['access_token']
-            head = {'Client-ID': self.client_id,
-                    'Authorization':  "Bearer " + token
-                    }
             content_json = requests.get(self.url, headers=head).json()["data"]
 
             if content_json:
                 content_json = content_json[0]
                 if content_json['type'] == 'live':
                     os.system("mpg123 " + "sound.mp3")
-                    return True
-                else:
-                    return False
-            else:
-                return False
         except requests.exceptions.ConnectionError:
-            self.check()
+            self.check(head)
 
 
 if __name__ == "__main__":
@@ -50,10 +49,11 @@ if __name__ == "__main__":
                                      any twitch streamer with a sound.""")
     parser.add_argument('--user', type=str, help="username of twitch streamer")
     args = parser.parse_args()
-    username = args.user
+    user_name = args.user
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit()
 
+    head_token = Notify(user_name).get_token()
     while True:
-        print(Notifly(username).check())
+        Notify(user_name).check(head_token)
