7c7aa3156d1160ba7e4c0d3eb5fe242cf79664e7
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-16 19:11:25 +0700

    Sleep the bot for 60 seconds after doing another request to Twitch

--- main.py
+++ main.py
@@ -17,35 +17,37 @@
 # along with twitch_notify.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
+import time
 import os
 import sys
 
 import requests
 from dotenv import load_dotenv
 
-load_dotenv('.env')
-SECRET = os.environ['SECRET']
-CLIENT_ID = os.environ['CLIENT_ID']
+load_dotenv(".env")
+SECRET = os.environ["SECRET"]
+CLIENT_ID = os.environ["CLIENT_ID"]
 
 
 class Notify:
     def __init__(self, username):
         self.username = username
-        self.url = f"https://api.twitch.tv/helix/streams?user_login={self.username}"
+        self.url = (
+            f"https://api.twitch.tv/helix/streams?user_login={self.username}"
+        )
         self.auth = "https://id.twitch.tv/oauth2/token"
         self.client_id = CLIENT_ID
         self.secret = SECRET
 
     def get_token(self):
-        auth_params = {'client_id': self.client_id,
-                       'client_secret': self.secret,
-                       'grant_type': 'client_credentials'
-                       }
+        auth_params = {
+            "client_id": self.client_id,
+            "client_secret": self.secret,
+            "grant_type": "client_credentials",
+        }
         aut_call = requests.post(url=self.auth, params=auth_params)
-        token = aut_call.json()['access_token']
-        head = {'Client-ID': self.client_id,
-                'Authorization': "Bearer " + token
-                }
+        token = aut_call.json()["access_token"]
+        head = {"Client-ID": self.client_id, "Authorization": "Bearer " + token}
         return head
 
     def check(self, head):
@@ -54,16 +56,19 @@ class Notify:
 
             if content_json:
                 content_json = content_json[0]
-                if content_json['type'] == 'live':
+                if content_json["type"] == "live":
                     os.system("mpg123 " + "sound.mp3")
+            time.sleep(60)
         except requests.exceptions.ConnectionError:
             self.check(head)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""This bot can notify you with
-                                     any twitch streamer with a sound.""")
-    parser.add_argument('--user', type=str, help="username of twitch streamer")
+    parser = argparse.ArgumentParser(
+        description="""This bot can notify you with
+                                     any twitch streamer with a sound."""
+    )
+    parser.add_argument("--user", type=str, help="username of twitch streamer")
     args = parser.parse_args()
     user_name = args.user
     if len(sys.argv) == 1:
